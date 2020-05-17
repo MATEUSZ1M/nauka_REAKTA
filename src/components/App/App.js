6483f26729a417d38f865dd04479430fc5588079
Mateusz Mikolajczyk <mikolajczykmateusz1993@gmail.com>
2020-05-17 21:26:14 +0000

    Add component Creator && component Button

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,25 +1,20 @@
 import React from "react";
 import styles from "./App.scss";
 import List from "../List/List.js";
+import { pageContents, listData } from "../../data/dataStore";
 
 class App extends React.Component {
   render() {
     return (
       <main className={styles.component}>
-        <h1 className={styles.title}>My first React app</h1>
-        <h2 className={styles.subtitle}>
-          A simple to-do app, with lists, columns and cards
-        </h2>
+        <h1 className={styles.title}>{pageContents.title}</h1>
+        <h2 className={styles.subtitle}>{pageContents.subtitle}</h2>
         <List
-          title={["Things to do", <sup key="1">soon!</sup>]}
-          heroImageSrc={
-            "http://uploads.kodilla.com/bootcamp/fer/11.react/space.png"
-          }
-        >
-          <p>
-            I'm planning on doing all these things sooner, ratcher than later!
-          </p>
-        </List>
+          title={listData.title}
+          description={listData.description}
+          image={listData.image}
+          columns={listData.columns}
+        />
       </main>
     );
   }
