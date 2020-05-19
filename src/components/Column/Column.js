c3124dc5fb21a89acc88b0d637f159979d63d53a
Mateusz Mikolajczyk <mikolajczykmateusz1993@gmail.com>
2020-05-19 13:28:03 +0000

    This is a combination of 3 commits.
    -Add component Icon
    
    -Add build version of project
    
    -Add ESlint and husky

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -4,6 +4,7 @@ import PropTypes from "prop-types";
 import Card from "../Card/Card";
 import Creator from "../Creator/Creator.js"
 import { settings } from "../../data/dataStore";
+import Icon from "../Icon/Icon";
 
 class Column extends React.Component {
   state = {
@@ -35,12 +36,13 @@ class Column extends React.Component {
 
 
   render() {
-    console.log('this', this);
-
     return (
       <section className={styles.component}>
         <h3 className={styles.title}>
           {this.props.title}
+          <span className={styles.icon}>
+            <Icon name={this.props.icon} />
+          </span>
         </h3>
        
        {
