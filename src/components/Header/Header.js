931442051e2b9810b581e5a32b11ed2623c06b2d
Mateusz Mikolajczyk <mikolajczykmateusz1993@gmail.com>
2020-05-26 13:40:28 +0000

    Add component Header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
+import styles from './Header.scss';
+import Container from '../Container/Container';
+import Icon from '../Icon/Icon';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header className={styles.component}>
+        <Container>
+          <div className={styles.wrapper}>
+            <Link to="/" className={styles.logo}>
+              <Icon name="bug" ActiveClassName="active" />
+            </Link>
+            <NavLink exact to="/">
+              Home
+            </NavLink>
+            <NavLink exact to="/info" ActiveClassName="active">
+              Info
+            </NavLink>
+          </div>
+        </Container>
+      </header>
+    );
+  }
+}
+
+export default Header;
--- src/components/MainLayout/MainLayout.js
+++ src/components/MainLayout/MainLayout.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import PropsTypes from 'prop-types';
-
+import Header from '../Header/Header';
 // eslint-disable-next-line
-const MainLayout = ({ children }) => <div>{children}</div>;
+const MainLayout = ({ children }) => <div>
+  <Header />
+  {children}</div>;
 
 MainLayout.propsTypes = {
   children: PropsTypes.node,
